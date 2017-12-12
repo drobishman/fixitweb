8dc27e0f8c6900b183ab1bdfff1237b12464174b
drobishman <adrian.drob88@gmail.com>
2017-12-12 15:29:56 +0200

    added methods to implement

--- src/main/java/fixit/dao/CarDao.java
+++ src/main/java/fixit/dao/CarDao.java
@@ -1,5 +1,14 @@
 package fixit.dao;
 
-public class CarDao {
+import java.util.List;
 
+public interface CarDao {
+
+   Car findById(int id);
+  
+   void save(Car car);
+  
+   void deleteById(Integer id)
+    
+   List<Car> findAllCars();
 }
