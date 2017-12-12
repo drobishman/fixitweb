b31b37c36b5bd980ebf46accd4833b5940441d75
drobishman <adrian.drob88@gmail.com>
2017-12-12 16:13:50 +0200

    implemented needed methods

--- src/main/java/fixit/dao/CarDaoImpl.java
+++ src/main/java/fixit/dao/CarDaoImpl.java
@@ -5,29 +5,45 @@ import java.util.List;
 import fixit.model.Car;
 
 public class CarDaoImpl extends AbstractDao<Integer, Car> implements CarDao {
+	
+	static final Logger logger = LoggerFactory.getLogger(UserDaoImpl.class);
 
 	@Override
 	public Car findById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Car car = getByKey(id);
+        if(car!=null){
+            Hibernate.initialize(car.getTroubleCodes);
+        }
+        return user;
 	}
 
 	@Override
 	public void save(Car car) {
-		// TODO Auto-generated method stub
-		
+		persist(car);
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		Criteria crit = createEntityCriteria();
+        	crit.add(Restrictions.eq("id", id));
+       		Car car = (Car)crit.uniqueResult();
+      		delete(car);
 		
 	}
 
 	@Override
 	public List<Car> findAllCars() {
-		// TODO Auto-generated method stub
-		return null;
+		Criteria criteria = createEntityCriteria().addOrder(Order.asc("registrationNumber"));
+        	criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);//To avoid duplicates.
+        	List<Car> cars = (List<Car>) criteria.list();
+         
+        // No need to fetch troubleCodes since we are not showing them on list page. Let them lazy load. 
+        // Uncomment below lines for eagerly fetching of userProfiles if you want.
+        /*
+        for(Car car : cars){
+            Hibernate.initialize(car.getTroubleCodes());
+        }*/
+        return users;
 	}
 
 }
