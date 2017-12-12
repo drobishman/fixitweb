1b1cba13d0dbf35429e287243bd4702f3e74f0ee
drobishman <adrian.drob88@gmail.com>
2017-12-12 14:01:41 +0200

    Update Car.java
    added its methods and attributes

--- src/main/java/fixit/model/Car.java
+++ src/main/java/fixit/model/Car.java
@@ -1,12 +1,82 @@
-/**
- * 
- */
 package fixit.model;
 
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+ 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotEmpty;
 /**
  * @author adrian
  *
  */
-public class Car {
+@Entity
+@Table(name="car")
+public class Car implements Serializable {
+  
+  @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+  private Integer id;
+  
+  @NotEmpty
+  @Column(name="registration_number", unique=true, nullable=false)
+  private String registrationNumber;
+  
+  @NotEmpty
+  @Column(name="chasis_number", nullable=false)
+  private String chasisNumber;
+  
+  @NotEmpty
+  @Column(name="brand", nullable=false)
+  private String brand;
+  
+  @NotEmpty
+  @Column(name="model", nullable=false)
+  private String chasisNumber;
+  
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((registrationNumber == null) ? 0 : registrationNumber.hashCode());
+        return result;
+    }
+  
+  @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Car))
+            return false;
+        Car other = (Car) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (registrationNumber == null) {
+            if (other.registrationNumber != null)
+                return false;
+        } else if (!registrationNumber.equals(other.registrationNumber))
+            return false;
+        return true;
+    }
+ 
+    @Override
+    public String toString() {
+        return "Car [id=" + id + ", registrationNumber=" + registrationNumber + ", chasisNumber="+ chasisNumber +", brand=" + brand +", model="+ model + "]";
+    }
 
 }
