0ab4ca8ff606370e17dcdfcbc49b9ee4f3aa2308
drobishman <adrian.drob88@gmail.com>
2017-12-13 13:20:18 +0200

    create car service implementation

---
+++ src/main/java/fixit/service/CarServiceImpl.java
@@ -0,0 +1,53 @@
+package fixit.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import fixit.dao.CarDao;
+import fixit.model.Car;
+ 
+ 
+@Service("carService")
+@Transactional
+public class CarServiceImpl implements CarService{
+
+    @Autowired
+    private CarDao dao;
+
+    public Car findById(int id){
+        return dao.findById(id);
+    }
+     
+    public void saveCar(Car car){
+        dao.save(car);
+    }
+     
+     
+     /*
+     * Since the method is running with Transaction, No need to call hibernate update explicitly.
+     * Just fetch the entity from db and update it with proper values within transaction.
+     * It will be updated in db once transaction ends. 
+     */
+    public void updateCar(Car car){
+       Car entity = dao.findById(car.getId());
+        if(entity!=null){
+            entity.setRegistrationNumber(car.getFirstRegistrationNumber());
+            entity.setChasisNumber(user.getChasisNumber());
+            entity.setBrand(user.getBrand());
+            entity.setModel(user.getModel());
+        }
+    }
+     
+    public void deleteCarById(Integer id){
+        dao.deleteById(id);
+    }
+ 
+    public List<Car> findAllCars(){
+        return dao.findAllCars();
+    }
+
+}
