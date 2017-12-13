266a35e262125b58fc11643b38d5df208a1efe02
drobishman <adrian.drob88@gmail.com>
2017-12-13 09:24:14 +0200

    implemented methods

--- src/main/java/fixit/dao/TroubleCodeDaoImpl.java
+++ src/main/java/fixit/dao/TroubleCodeDaoImpl.java
@@ -5,36 +5,41 @@ import java.util.List;
 import fixit.model.TroubleCode;
 import fixit.model.User;
 
-public class TroubleCodeDaoImpl implements TroubleCodeDao{
+public class TroubleCodeDaoImpl extends AbstractDao<Integer, TroubleCode> implements TroubleCodeDao{
 
 	@Override
-	public TroubleCode findById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public TroubleCode findById(int id){
+		TroubleCode troubleCode = getByKey(id);
+        	return troubleCode;
 	}
 
 	@Override
 	public User findByNumber(String number) {
-		// TODO Auto-generated method stub
-		return null;
+		TroubleCode troubleCode = getByNumber(id);
+        	return troubleCode;
 	}
 
 	@Override
 	public void save(TroubleCode troubleCode) {
-		// TODO Auto-generated method stub
+		persist(troubleCode);
 		
 	}
 
 	@Override
 	public void deleteByNumber(String number) {
-		// TODO Auto-generated method stub
+		Criteria crit = createEntityCriteria();
+        	crit.add(Restrictions.eq("number", number));
+       		TroubleCode troubleCode = (TroubleCode)crit.uniqueResult();
+      		delete(troubleCode);
 		
 	}
 
 	@Override
 	public List<TroubleCode> findAllTroubleCodes() {
-		// TODO Auto-generated method stub
-		return null;
+		Criteria criteria = createEntityCriteria().addOrder(Order.asc("number"));
+        	criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);//To avoid duplicates.
+        	List<TroubleCode> troubleCodes = (List<TroubleCodes>) criteria.list();
+        	return troubleCodes;
 	}
 
 }
